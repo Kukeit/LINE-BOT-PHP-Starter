9fec26b50074c5595da93713de1d58ba3a2ec7ec
Kukeit Chanahan <kukeitch@yipintsoigroup.com>
2017-11-13 11:35:00 +0700

    first

--- bot.php
+++ bot.php
@@ -6,13 +6,19 @@ include ('line-bot.php');
 $channelSecret = 'b252de3ae956da991dcad579fcc38e5a';
 $access_token  = 'DsIQv47M0kuBZMWl4pDyrFHYBjkC7rSnDS1ByA4+9WnHoT215FkNOxoxo6p5gsmeAHLCOg1Nc7NYkmR6W7Rb8me9pJb5egSxP5xvcYj6HYxkXF8E5xJEOKo/660uBoAWPLF9R6lvcz//UCnCWQ5UqwdB04t89/1O/w1cDnyilFU=';
 
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('U2b250e9c1c5e8c3793cf0a9f053e0b25', $textMessageBuilder);
+$response = $bot->replyMessage($arrJson['events'][0]['replyToken'], $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+
 
 
 ?>
