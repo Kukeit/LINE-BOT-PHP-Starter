4e498b9358d511d4e6a546404b17c30dee3d32bb
Kukeit Chanahan <kukeitch@yipintsoigroup.com>
2017-11-13 14:30:33 +0700

    first

--- bot.php
+++ bot.php
@@ -3,6 +3,9 @@
 
 include ('line-bot.php');
 use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
 
 $channelSecret = 'b252de3ae956da991dcad579fcc38e5a';
 $access_token  = 'DsIQv47M0kuBZMWl4pDyrFHYBjkC7rSnDS1ByA4+9WnHoT215FkNOxoxo6p5gsmeAHLCOg1Nc7NYkmR6W7Rb8me9pJb5egSxP5xvcYj6HYxkXF8E5xJEOKo/660uBoAWPLF9R6lvcz//UCnCWQ5UqwdB04t89/1O/w1cDnyilFU=';
