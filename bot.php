b95a03444828400d590b0d1f5baf93e982e835c7
Kukeit Chanahan <kukeitch@yipintsoigroup.com>
2017-11-13 10:33:21 +0700

    2

--- bot.php
+++ bot.php
@@ -1,28 +1,3 @@
-<?php 
-
-
-$access_token = 'DsIQv47M0kuBZMWl4pDyrFHYBjkC7rSnDS1ByA4+9WnHoT215FkNOxoxo6p5gsmeAHLCOg1Nc7NYkmR6W7Rb8me9pJb5egSxP5xvcYj6HYxkXF8E5xJEOKo/660uBoAWPLF9R6lvcz//UCnCWQ5UqwdB04t89/1O/w1cDnyilFU=';
-
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-
-			
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			$contents = file_get_contents('./curl.sh '.$access_token.' '.$replyToken);
-			echo shell_exec($contents);
-		}
-	}
-}
-echo 'K1';
+<?php
+	echo "Hello Line";
 ?>
